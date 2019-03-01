e45f7cd9b32f11ec3dffb7f3aa7a0130fbb43ced
rashmiap <rashmiap.10@gmail.com>
2019-03-01 09:56:13 +0530

    GET method to display result orderby need to work on it

--- app/router/poll.router.js
+++ app/router/poll.router.js
@@ -9,64 +9,57 @@ router.post('/poll', VerifyToken, function(req, res, next){
   const body = req.body;
   if(body.UserId1 && body.DishId1){
     User.findOneAndUpdate(
-      {
-        _id: body.UserId1,
-        'dish._id': body.DishId1
-      },
-      {
-        $set: {
-          'dish.$.dishPollCount': 30
-        }
-      }, null,
+      {_id: body.UserId1,'dish._id': body.DishId1},
+      {$inc: {'dish.$.dishPollCount': 30}},
+      {new: true},
       (err, doc)=> {
         if (err) {
           return res.status(500).send(err, "There was a problem updating dish poll - 1st ");
         }
-
       })
   }
   if(body.UserId2 && body.DishId2){
-
     User.findOneAndUpdate(
-      {
-        _id: body.UserId2,
-        'dish._id': body.DishId2
-      },
-      {
-        $set: {
-          'dish.$.dishPollCount': 20
-        }
-      }, null,
+      {_id: body.UserId2,'dish._id': body.DishId2},
+      {$inc: {'dish.$.dishPollCount': 20}},
+      {new: true},
       (err, doc)=> {
         if (err) {
           return res.status(500).send(err, "There was a problem updating dish poll - 2md ");
         }
-
       })
   }
   if(body.UserId3 && body.DishId3){
-
     User.findOneAndUpdate(
-      {
-        _id: body.UserId3,
-        'dish._id': body.DishId3
-      },
-      {
-        $set: {
-          'dish.$.dishPollCount': 10
-        }
-      }, null,
+      {_id: body.UserId3,'dish._id': body.DishId3},
+      {$inc: {'dish.$.dishPollCount': 10}},
+      {new: true},
       (err, doc)=> {
         if (err) {
           return res.status(500).send(err, "There was a problem updating dish poll - 3rd ");
         }
-
       })
   }
-
-    res.json({
-      "dish": "fo"
-    })
+  res.json({
+    "data": "poll updated"
+  })
 })
 
+// GET all dishes from all users
+router.get('/result', VerifyToken, function(req, res, next) {
+
+  User.find(
+    {},
+    // {$orderby: {'dish.$.dishPollCount': -1}},
+    (err, doc) => {
+      if (err) {
+        return res.status(500).send(err, "There was a problem updating dish poll - 3rd ");
+      }
+      else {
+        return res.json({
+          "datadd": doc
+        })
+      }
+    })
+});
 module.exports = router;
